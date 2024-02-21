3ec0185f067b94b7378f5a6c3d4abdc35007ec63
amaros <amaros@amaros-osx.som.umaryland.edu>
2024-02-21 12:32:36 -0500

    first commit

---
+++ pages/1_📊_MgSs_Analysis.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+st.set_page_config(layout="centered")
+
+clust = pd.read_csv("Data/mgss.clustering.parameters.csv").rename(columns={"Unnamed: 0" : "species"})
+mgss_coverage = pd.read_csv("Data/mgSs.coverage.stats.csv").rename(columns={"Unnamed: 0" : "sub_species", "as.factor(sample_cluster)" : "sample_cluster"})
+species = mgss_coverage['sub_species'].apply(lambda x : x.split(".")[0]).unique()
+mgss_candidates = pd.read_csv("Data/potential.mgss.candidates.csv")
+
+
+st.title("Metagenomic Subspecies")
+
+st.subheader("Visualizations")
+
+
+a = len(species)
+st.write(a)
+option = st.selectbox("Species", species)
+tab1, tab2, tab3 = st.tabs(["Species coverage", "Subspecies stats", "Presence Absence heatmap"])
+
+with tab1:
+        
+        col1, col2 = st.columns(2)
+        with col1 :
+            fig1 = Image.open("Medias/mgss_coverage_png/" + option + "_subspecies_coverage_boxplot.png") 
+            st.image(fig1)
+
+        with col2 :
+            fig2 = Image.open("Medias/mgss_coverage_png/" + option + "_subspecies_coverage_by_NoGenes.png")
+            st.image(fig2)
+
+with tab2 :
+
+        st.subheader("Clustering parameters")
+        df2 = clust[clust['species'] == option].drop('Number_of_unassigned', axis = 1)
+        st.dataframe(df2)
+        
+        st.subheader("Subspecies stats")
+        st.dataframe(mgss_coverage[mgss_coverage['sub_species'].apply(lambda x : x.split(".")[0]) == option])
+
+with tab3 :
+        image = Image.open("Medias/heatmap_presence_absence/_" + option + "_heatmap_presence_absence.png")
+        st.image(image)
+
+
+# st.subheader("Samples composition")
+# st.caption("Percentage of samples containing more than a specific percentage (50-60-70-80-90) of protein count")
+
+# but1, but2 = st.columns(2)
+# with but1:
+#        hide = st.button("Considered specie only")
+# with but2:
+#        show = st.button("All species")
+
+# # Function to display DataFrame based on button click
+# def display_data(df):
+#     if show == True:
+#         st.dataframe(df.set_index('species'))
+#     elif hide == True:
+#         st.dataframe(df[df['species']==option].set_index('species'))
+#     else:
+#         st.dataframe(df.set_index('species'))
+
+# # Call function to display data
+# display_data(mgss_candidates)
---
+++ pages/2_📚_MgCST_clustering.py
@@ -0,0 +1,159 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import subprocess
+import base64  
+import plotly.express as px
+import numpy as np
+
+st.set_page_config(layout="wide")
+
+# subspecies_with_colors = pd.read_csv("Data/subspecies_with_colors.csv")
+mgcsts_samples_df = pd.read_csv("Data/mgCSTs.samples.df.csv")
+mgCSTs_sort = pd.read_csv("Data/mgCST_sort_color.csv")
+projects = pd.read_csv("Data/VIRGO2_projects.csv")
+
+
+# Slider for parameters variation
+st.sidebar.subheader("Parameters")
+parameters = pd.read_csv("Data/mgCSTs_parameters_streamlit.csv")
+value_ds = parameters['deepsplit'].values[0]
+value_mcs = parameters['minClusterSize'].values[0]
+deepsplit = st.sidebar.slider(label="deepSplit", min_value=0, max_value=4, value = value_ds)
+minclustersize = st.sidebar.slider(label="minClusterSize", min_value=10, max_value=50, value = value_mcs)
+parameters = pd.DataFrame({"minClusterSize" : [minclustersize], "deepsplit" : [deepsplit]})
+parameters.to_csv("Data/mgCSTs_parameters_streamlit.csv", index = False)
+
+mgcsts_samples = mgcsts_samples_df[(mgcsts_samples_df['deepSplit'] == deepsplit) & (mgcsts_samples_df['minClusterSize'] == minclustersize)]
+mgcsts_samples = mgcsts_samples.reset_index(drop = True)
+mgcsts_samples = mgcsts_samples.merge(projects, on = "sampleID", how = "left")
+
+mgcsts = mgCSTs_sort[(mgCSTs_sort['deepSplit'] == deepsplit) & (mgCSTs_sort['minClusterSize'] == minclustersize)]
+mgcsts = mgcsts.reset_index(drop = True)
+
+count_sample = []
+for element in mgcsts['dtc'].values :
+    count_sample.append(mgcsts_samples.groupby(['dtc']).count()['sampleID'][element])
+mgcsts['count_sample'] = count_sample
+
+st.container()
+st.subheader("Clustering parameters")
+
+mgcsts['mgCST'] = mgcsts['mgCST'].astype(str)
+
+col1, col2, col3 = st.columns(3)
+with col1 :
+    fig = px.bar(mgcsts, x='mgCST', y='count_sample', color='mgCST',
+                 hover_data=['domTaxa'],
+                 color_discrete_sequence=list(mgcsts['color_mgCST']), 
+                 labels={"count_sample" : "Number of samples"},
+                 title = "Number of samples on each MgCSTs - colored by mgCST")
+    subtitle_text = f"MinClusterSize = {minclustersize} <br> DeepSplit = {deepsplit}"
+    fig.add_annotation(
+        dict(text=subtitle_text,
+            xref='paper', yref='paper',
+            x=0, y=1.05,
+            showarrow=False,
+            font=dict(size=12)
+            ))
+
+    fig.update_xaxes(tickmode='array', tickvals=mgcsts['mgCST'])
+    fig.update_layout(
+    xaxis=dict(
+        tickangle=0,  # Change the angle of the tick labels
+        tickfont=dict(size=10)  # Change the size of the tick labels
+        )
+        )
+    st.plotly_chart(fig, use_container_width=True)
+
+with col2 :
+    # Project Colors
+    proj_cols = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#d1ba36', '#a65628', '#f781bf', '#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e']
+    df2 = mgcsts_samples.groupby(["Project", "mgCST"]).size().reset_index().pivot(columns='Project', index = 'mgCST', values =0).reset_index()
+
+    fig = px.bar(df2, x='mgCST', y=df2.drop('mgCST', axis=1).columns.values, labels={'value' : 'Number of samples'},
+                 title = f"Number of samples on each MgCSTs - colored by projects")
+    subtitle_text = f"MinClusterSize = {minclustersize} <br> DeepSplit = {deepsplit}"
+    fig.add_annotation(
+        dict(text=subtitle_text,
+            xref='paper', yref='paper',
+            x=0, y=1.05,
+            showarrow=False,
+            font=dict(size=12)
+            )
+    )
+    fig.update_xaxes(tickmode='array', tickvals=mgcsts['mgCST'])
+    fig.update_layout(
+    xaxis=dict(
+        tickangle=0,  # Change the angle of the tick labels
+        tickfont=dict(size=10)  # Change the size of the tick labels
+        ))
+    st.plotly_chart(fig, use_container_width=True)
+
+with col3 :
+    File_S6 = pd.read_excel('Data/File_S6_clean.xlsx')
+    old_mgCST = File_S6[['mapID', 'mgCST']]
+    old_mgCST = old_mgCST.rename(columns={'mapID':'sampleID'})
+    new_mgCST = mgcsts_samples[(mgcsts_samples['deepSplit'] == deepsplit) & (mgcsts_samples['minClusterSize'] == minclustersize)][['sampleID', 'mgCST']]
+    new_vs_old = pd.merge(new_mgCST, old_mgCST, on='sampleID', how='inner')
+    bubble_data = new_vs_old.groupby(['mgCST_x', 'mgCST_y']).size().reset_index(name='count')
+    bubble_data = bubble_data.rename(columns={"mgCST_x":"mgCST", "mgCST_y":"old_mgCST"})
+
+    bubble_color = []
+    mgcsts['mgCST'] = mgcsts['mgCST'].astype(int)
+    for i in sorted(bubble_data['mgCST'].unique()):
+        bubble_color.append(mgcsts[mgcsts['mgCST'] == i]['color_mgCST'].values[0])
+
+    bubble_data['mgCST'] = bubble_data['mgCST'].astype(str)
+    fig = px.scatter(bubble_data, x='mgCST', y = 'old_mgCST', color='mgCST',color_discrete_sequence=list(bubble_color), size='count',
+                     title = "Number of samples in previous MgCST vs New MgCST")
+    subtitle_text = f"MinClusterSize = {minclustersize} <br> DeepSplit = {deepsplit}"
+    fig.add_annotation(
+        dict(text=subtitle_text,
+            xref='paper', yref='paper',
+            x=0, y=1.05,
+            showarrow=False,
+            font=dict(size=12)
+            )
+    )
+    fig.update_xaxes(tickmode='array', tickvals=mgcsts['mgCST'])
+    fig.update_yaxes(tickmode='array', tickvals=np.arange(1,28))
+    fig.update_layout(
+    xaxis=dict(
+        tickangle=0,  # Change the angle of the tick labels
+        tickfont=dict(size=10)  # Change the size of the tick labels
+        ),
+    yaxis=dict(
+        tickangle=0,  # Change the angle of the tick labels
+        tickfont=dict(size=10)  # Change the size of the tick labels
+        ),
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
+
+st.subheader("Most abund species per mgCSTs")
+with st.expander("Show table"):
+    st.dataframe(mgcsts[['mgCST','domTaxa','meanRelabund','color_mgCST']])
+
+st.subheader("MgCSTs heatmap")
+
+heatmap = st.button(label = "Generate heatmap (1min30)")
+if heatmap :
+    st.subheader("MgCSTs Heatmap")
+
+    process = subprocess.Popen(["Rscript", "mgCSTs_heatmap.R"])
+    result = process.communicate()
+
+    def displayPDF(file):
+        # Opening file from file path
+        with open(file, "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+        # Embedding PDF in HTML
+        pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+
+        # Displaying File
+        st.markdown(pdf_display, unsafe_allow_html=True)
+
+    displayPDF("Medias/mgCST_heatmap.pdf")
